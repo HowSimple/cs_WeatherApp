bee0c6b3b0c07baba7666b8f7747057a01396fde
HowSimple <43933950+HowSimple@users.noreply.github.com>
2019-08-15 18:49:58 -0500

    Implementing storage response for textbox

--- Models/WeatherResponse.cs
+++ Models/WeatherResponse.cs
@@ -1,7 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace WeatherApp.Models
 {
+	public static class Repository
+	{
+		private static List<WeatherResponse> responses = new List<WeatherResponse>( );
+
+		public static IEnumerable<WeatherResponse> Responses
+		{
+			get { return responses; }
+		}
+		public static void AddResponse(WeatherResponse response)
+		{
+			responses.Add(response);
+		}
+	}
+
+
 	public class WeatherResponse
 	{
 		[Required(ErrorMessage = "Please enter your zipcode")]
