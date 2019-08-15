212571453710f1f5230fc2261b7f9b58a172eef0
HowSimple <caleb@again.email>
2019-08-15 12:17:07 -0500

    More work on setting up textbooks and submitting

---
+++ Models/WeatherResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherApp.Models
+{
+	public class WeatherResponse
+	{
+		[Required(ErrorMessage = "Please enter your zipcode")]
+		public string Zipcode { get; set; }
+
+	}
+}
