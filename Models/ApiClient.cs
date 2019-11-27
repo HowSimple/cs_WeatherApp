67fd0f16a4f8b332ac5cc05e1653eab148f0b996
HowSimple <43933950+HowSimple@users.noreply.github.com>
2019-11-26 18:25:05 -0600

    Implemented weather condition icons

--- Models/ApiClient.cs
+++ Models/ApiClient.cs
@@ -2,24 +2,41 @@
 using System.Net;
 using WeatherApp.Properties;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
 
 namespace WeatherApp.Models
 {
-	public partial class ApiClient
+	public class ApiClient
 	{
 		public ApiData data;
+		public string icon;
 
+		public string getApiUri(string zipcode, string units = "imperial")
+		{
+			string apikey = Resources.API_KEY; // OpenWeatherMaps API Key
+			string uri = "http://api.openweathermap.org/data/2.5/weather?zip="
+					+ zipcode + ",US&APPID=" + apikey + "&units="+ units;
+			return uri;
+		}
+		public string getIconUri()
+		{
+			string iconID = data.weather[0].icon;		
+			string uri = "http://openweathermap.org/img/wn/"
+				+iconID+"@2x.png";
+			return uri;
+			
+		}
 		public ApiClient(string zipcode)
 		{
 			try
 			{
-				string apikey = Resources.API_KEY;
-				string uri = "http://api.openweathermap.org/data/2.5/weather?zip="
-							 + zipcode + ",US&APPID=" + apikey + "&units=imperial";
+				string uri = getApiUri(zipcode); 
 				WebClient client = new WebClient( );
 				string json = client.DownloadString(uri);
 				data = JsonConvert.DeserializeObject<ApiData>(json);
-
+				icon = getIconUri();
 			}
 			catch (WebException excepetion)
 			{
